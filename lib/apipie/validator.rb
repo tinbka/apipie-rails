fe93cd643a581ce5146ea33070693115245ec4c8
Sergey Baev <sbaev@at-consulting.ru>
2016-11-09 17:21:13 +0300

    Fixed ActionController::Parameters is no longer a Hash

--- lib/apipie/validator.rb
+++ lib/apipie/validator.rb
@@ -302,7 +302,7 @@ module Apipie
       end
 
       def validate(value)
-        return false if !value.is_a? Hash
+        return false if !value.respond_to? :keys
         if @hash_params
           @hash_params.each do |k, p|
             if Apipie.configuration.validate_presence?
